eba995abfe84417e4e4dcf2065dfce7d43f47cab
Dhruv Bhola <dharuvbhola@gmail.com>
2023-02-24 19:33:55 +0530

    Optimise Code

--- tut5.cpp
+++ tut5.cpp
@@ -7,6 +7,7 @@ int main(){
     cout<<"Enter the value of num1:\n"; /* '<<' is called Insertion operator */
     cin>>num1; /* '>>' is called Extraction operator*/
 
+
     cout<<"Enter the value of num2:\n"; /* '<<' is called Insertion operator */
     cin>>num2; /* '>>' is called Extraction operator*/
 
